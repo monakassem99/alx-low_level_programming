ac4da1c3fceddfbf7eceb9f6bc278636d0ab7ee3
monakassem99 <mona.kassem9999@gmail.com>
2023-03-16 17:52:01 -0700

    101-quote.c-1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fgets("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
