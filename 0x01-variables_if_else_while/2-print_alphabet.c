98295bca4664de2edd3e75973aa48fe4e4d4b528
monakassem99 <mona.kassem9999@gmail.com>
2023-03-17 18:35:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		printf("%c ", ch);
+	}
+	return 0;
+}
