8df36e460b1c3f68e4c7aa83946b0fe7ae1ab528
monakassem99 <mona.kassem9999@gmail.com>
2023-03-18 05:56:20 -0700

    6-print_numberz.c-2

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,11 @@
 int main (void)
 {
 	int num;
-	for(num = '0'; num <= '9'; num++)
+
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar (num);
+		putchar(num);
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
