233010dfb02eb9f7c22c8d5983a5d4883b7148fb
monakassem99 <mona.kassem9999@gmail.com>
2023-03-21 16:26:08 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+
 void print_alphabet_x10(void)
 {
+	int ch, count;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+			_putchar('\n');
+		}
+	}
 
 }
