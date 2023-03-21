6363b7dd15334c48fcbfb89ef24f98c16cca9c9a
monakassem99 <mona.kassem9999@gmail.com>
2023-03-21 14:38:05 -0700

    0-putchar.c-7

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-	printf ("_putchar\n");
+	printf("_putchar\n");
 	return (0);
 }
