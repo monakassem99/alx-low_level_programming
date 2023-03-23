449929d7518dd2e3a3a6f1da67170f291ca8292b
monakassem99 <mona.kassem9999@gmail.com>
2023-03-23 11:53:50 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,22 +4,12 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-void positive_or_negative(int n)
-{
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
-	else
-		printf("%i is negative\n", n);
-
-}
 
 int main(void)
 {
         int i;
 
-        i = 0;
+        i = 98 % 2;
         positive_or_negative(i);
 
         return (0);
