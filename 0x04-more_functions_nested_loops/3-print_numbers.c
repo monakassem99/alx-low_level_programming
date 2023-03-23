73e85f4e1985e31680e1f1347a94411829bdd96a
monakassem99 <mona.kassem9999@gmail.com>
2023-03-23 16:49:26 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers 
+ * numbers range 0 - 9
+ * return 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+	return (0);
+}
