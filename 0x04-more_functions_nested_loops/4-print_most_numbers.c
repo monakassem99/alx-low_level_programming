6e014a7b2426901a25b71a20b23dee055c3db53a
monakassem99 <mona.kassem9999@gmail.com>
2023-03-24 05:40:00 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  * print_most_numbers - print 0 -9
- * 		from 2 and 4 and you can
- * 		only use _putchar twice
+ *		from 2 and 4 and you can
+ *		only use _putchar twice
  * Return : Always 0 (success)
  */
 void print_most_numbers(void)
