0c95feab48beb891f270c1942e49c7fca5b5792d
monakassem99 <mona.kassem9999@gmail.com>
2023-03-26 15:38:30 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- * jack_bauer function prints every
- *		minute of the day
+ * jack_bauer - prints every minute of the day of Jack Bauer
  *
- *@a is the hours and
- *@b is the minutes
- * Return : always 0
+ * Return : ....
  */
 
 void jack_bauer(void)
