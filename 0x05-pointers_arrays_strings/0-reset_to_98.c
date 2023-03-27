b950470b70d438c11d6207cb20814c34d2120860
monakassem99 <mona.kassem9999@gmail.com>
2023-03-27 07:07:58 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	n = 402;
+	_putchar('n');
+	_putchar(=);
+	_putchar(n);
+
+	*n = 98;
+	_putchar('n');
+	_putchar(=);
+	_putchar(*n);
+
+}
