29f52857ad503fd8d2a6a06264c936c2363738a7
monakassem99 <mona.kassem9999@gmail.com>
2023-03-26 19:25:34 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,41 +7,36 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n >= 0 && n <= 98)
+	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n <= 98; n++)
 		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
-		printf("\n");
 	}
-	else if (n < 0)
+	else
 	{
-		for (i = n; i <= 98; i++)
+		for (; n >= 98; n--)
 		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
-		printf("\n");
 	}
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
-		printf("\n");
-	}
-	else if (n == 98)
-	{
-			printf("%d", n);
-	}
-		printf("\n");
 }
