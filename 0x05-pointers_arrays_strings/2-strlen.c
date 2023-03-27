07da42a22445bb7f21fd6e8b5d33c49af6bfd3b2
monakassem99 <mona.kassem9999@gmail.com>
2023-03-27 14:43:00 -0700

    11-print_to_98.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
+/**
+ * _strlen - prints the lenght of given string
+ *
+ * @s: the pointer of string
+ * Return: length of string
+ */
+
 int _strlen(char *s)
 {
 	int n;
 	int i = 0;
 
-	for (n = 0; s [n] != '\0'; n++)
+	for (n = 0; s[n] != '\0'; n++)
 	{
 		i++;
 	}
