5fcc9cb35fa87b3ba7e877f974e60c1da8264f13
monakassem99 <mona.kassem9999@gmail.com>
2023-03-30 16:01:24 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: on success 1.
+ * 	   on error. -1.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
