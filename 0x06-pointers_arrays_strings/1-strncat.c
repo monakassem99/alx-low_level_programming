89cb8e67a1a3563f2b33258b35692e909a84a048
monakassem99 <mona.kassem9999@gmail.com>
2023-03-30 16:08:18 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate 2 strings
+ * @dest: destination string
+ * @src: the source string
+ * @n: number oof characters
+ * return : concatenated strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+	for (i = 0; i < n ; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
