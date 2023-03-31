aa8c5176f150772fa40e5db31ec8575cda0316d9
monakassem99 <mona.kassem9999@gmail.com>
2023-03-31 13:16:19 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,25 @@
 void times_table(void)
 {
 	int a, b, c;
-	
+
 	for (a = 0; a <= 9; a++)
 	{
 		putchar('0');
-		
+
 		for (b = 1; b <= 9; b++)
 		{
 			putchar(',');
 			putchar(' ');
-			
+
 			c = a * b;
-			
+
 			if (c <= 9)
 				putchar(' ');
 			else
 				putchar((c / 10) + '0');
 			putchar((c % 10) + '0');
 		}
-		
+
 		putchar('\n');
 	}
 }
