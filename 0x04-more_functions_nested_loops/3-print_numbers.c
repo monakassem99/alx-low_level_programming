ea7c49f5d7189a8cae00e295ee583308b0dccd46
monakassem99 <mona.kassem9999@gmail.com>
2023-04-04 04:17:17 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,10 @@
 
 void print_numbers(void)
 {
-	int n = 0;
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
 
-	do {
-	
-		_putchar(n + 48);
-		n++;
-	} while (n >= 0 && n <= 9);
-	_putchar('\n')
+	_putchar('\n');
 }
