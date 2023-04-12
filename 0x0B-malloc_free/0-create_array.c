e977458fe1921957f63d71b069f55c00a6e9f827
monakassem99 <mona.kassem9999@gmail.com>
2023-04-11 18:44:38 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,11 @@ char *create_array(unsigned int size, char c)
 {
 	char *x = malloc(size);
 
-	if (size == 0 || n == 0)
+	if (size == 0 || x == 0)
 		return (0);
 
 	while (size--)
 		x[size] = c;
 
-	return (n);
+	return (x);
 }
