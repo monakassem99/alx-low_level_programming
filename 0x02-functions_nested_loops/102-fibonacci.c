6efa382dc0f39319a28f7904f22946068352ad44
monakassem99 <mona.kassem9999@gmail.com>
2023-07-13 06:58:27 -0700

    edited task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,11 +10,14 @@ int main(void)
 	int i;
 	long int fibb[50];
 
-	fibb[0] = 0;
-	fibb[1] = 1;
+	fibb[0] = 1;
+	fibb[1] = 2;
 	for (i = 2; i < 50; i++)
 	{
 		fibb[i] = fibb[i - 1] + fibb[i - 2];
+	}
+	for (i = 0; i < 50; i++)
+	{
 		printf("%ld", fibb[i]);
 		if (fibb[i] != fibb[49])
 		{
