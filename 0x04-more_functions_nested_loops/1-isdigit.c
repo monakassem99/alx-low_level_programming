42863179f0fc629312efbea48978f7adc976ee7e
monakassem99 <mona.kassem9999@gmail.com>
2023-07-13 12:53:50 -0700

    modefied

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -11,7 +11,11 @@
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,11 @@ void more_numbers(void)
 		for (n = 0; n <= 14; n++)
 		{
 			if (n > 9)
-
-			putchar((n / 10) + '0');
-			putchar((n % 10) + '0');
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
