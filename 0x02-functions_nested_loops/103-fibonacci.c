016cfb555cdc7ae2fd0beea7b47d8a0175704830
monakassem99 <mona.kassem9999@gmail.com>
2023-07-13 08:32:30 -0700

    new task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+ *
+ */
+int main(void)
+{
+	int i;
+	long long int fibb[50];
+	long int sum;
+
+	sum = 0;
+	fibb[0] = 1;
+	fibb[1] = 2;
+
+	for (i = 2; i < 50; i++)
+	{
+		fibb[i] = fibb[i - 1] + fibb[i - 2];
+		if (fibb[i] > 4000000)
+		{
+			break;
+		}
+		if (fibb[i] % 2 == 0)
+		{
+			sum = sum + fibb[i];
+		}
+	}
+	printf("%ld", sum);
+	printf("\n");
+
+	return (0);
+}
