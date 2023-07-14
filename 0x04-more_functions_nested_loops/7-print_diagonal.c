35add0892ebf5efe5f31d585011f48d1811078af
Mona Ahmed <mona.kassem9999@gmail.com>
2023-07-14 14:56:06 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,20 @@
 
 void print_diagonal(int n)
 {
-	int i,j;
+	int i, j;
+	
 	if (n <= 0)
 	{
-			_putchar('\n');
-			return;
+		_putchar('\n');
+		return;
 	}
 	for (i = 0; i < n; i++)
 	{
-			for (j = 0; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
-					_putchar(' ');
+			_putchar(' ');
 		}
-			_putchar('\\');
-			_putchar('\n');
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
