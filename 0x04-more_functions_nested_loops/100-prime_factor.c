9fb8037c9fb7d445a3094d25aced0f8e9492d663
monakassem99 <mona.kassem9999@gmail.com>
2023-07-20 10:08:19 -0700

    uodated

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,13 +4,15 @@ int main(void)
 {
 	long int n = 612852475143;
 	long int largest_factor = 1;
+	long int i;
 
-	for (long int i = 2; i <= n; i++)
+	for (i = 2; i <= n; i++)
 	{
 		if (n % i == 0)
 		{
 			int is_prime = 1;
 			long int j;
+
 			for (j = 2; j <= i / 2; j++)
 			{
 				if (i % j == 0)
