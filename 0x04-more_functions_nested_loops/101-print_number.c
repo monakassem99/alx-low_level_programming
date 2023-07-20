6489533a97043d0d9ab0b722fe3ddf36be0bd597
monakassem99 <mona.kassem9999@gmail.com>
2023-07-20 12:19:50 -0700

    uodated

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,15 +8,19 @@
 
 void print_number(int n)
 {
+	unsigned int i;
+
+	i = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -i;
 	}
-	if (n / 10 != 0)
+	if (i / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(i % 10 + '0');
 }
 
