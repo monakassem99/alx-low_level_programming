1edac341c9214576da15af30de4c6f40d643b0dd
monakassem99 <mona.kassem9999@gmail.com>
2023-07-21 11:53:21 -0700

    updated

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,22 +9,15 @@
  *
  * Return: the pointer to dest
  */
+
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int i;
 
-	len = 0;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-
 	return (dest);
 }
