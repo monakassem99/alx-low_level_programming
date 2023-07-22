2483312b9996420506fd370c00fa69134213967e
monakassem99 <mona.kassem9999@gmail.com>
2023-07-22 12:59:48 -0700

    updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0';)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
