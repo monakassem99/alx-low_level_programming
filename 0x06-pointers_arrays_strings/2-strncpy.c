fbd706177252159febe3f6b70d02b23fe70de897
monakassem99 <mona.kassem9999@gmail.com>
2023-07-23 14:11:42 -0700

    hello

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncpy - copies string
  * @dest: destination string
@@ -13,11 +14,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	while (i < n)
 	{
-		dest[i] = '\0';
-		i++;
+		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
