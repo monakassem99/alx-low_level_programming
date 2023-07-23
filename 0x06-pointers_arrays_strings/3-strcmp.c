ccf854b49c2d59664b1491a95b025b7289fb6bec
monakassem99 <mona.kassem9999@gmail.com>
2023-07-23 15:14:00 -0700

    hello

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,4 @@
 #include "main.h"
-
-/**
- * _strlen - prints the lenght of given string
- *
- * @s: the pointer of string
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		len++;
-	}
-	return (len);
-}
-
 /**
  * _strcmp - compares 2 strings
  * @s1: string 1
@@ -31,5 +11,6 @@ int _strcmp(char *s1, char *s2)
 	for (; *s1 == *s2; s1++, s2++)
 		if (*s1 == '\0')
 			return (0);
-	return (*(unsigned char *)s1 < *(unsigned char *)s2 ? -15 : 15);
+	return (*(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1);
 }
+
