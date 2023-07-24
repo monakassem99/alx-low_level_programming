62b05205758d85111d29e1b420d28e345925644c
monakassem99 <mona.kassem9999@gmail.com>
2023-07-24 09:57:23 -0700

    updated

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,14 +9,13 @@
 
 char *string_toupper(char *str)
 {
+
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-		{
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
-		}
 	}
 	return (str);
 }
