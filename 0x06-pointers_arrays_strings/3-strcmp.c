84888904e0faba28310e4aa680ddf3d2361cc815
monakassem99 <mona.kassem9999@gmail.com>
2023-07-25 11:30:23 -0700

    updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 && (*s1 == *s2); s1++, s2++)
-		;
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (s1[i] - s2[i]);
 }
