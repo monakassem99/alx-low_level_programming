e202d7f161a07321c927067b148a1a6097a2872e
monakassem99 <mona.kassem9999@gmail.com>
2023-08-02 23:13:16 +0300

    modified

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
+/**
+ * wildcamp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+ */
+
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	else if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else
+	{
+		return (0);
+	}
 }
-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
