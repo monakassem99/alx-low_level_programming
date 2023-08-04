fec765300ee1d3617e9d4f1faae83c018fc51814
monakassem99 <mona.kassem9999@gmail.com>
2023-08-04 20:53:30 +0300

    updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
