a3aa521fb00c8980befa6bb711864bc975c02982
monakassem99 <mona.kassem9999@gmail.com>
2023-08-07 22:27:01 +0300

    updated

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a new string
+ * which is a duplicate of the string str
+ * @str: string
+ * Return: pointer to the duplicated string
+ * and NULL if insufficient memory was available
+ */
+
+char *_strdup(char *str)
+{
+	size_t len = strlen(str) + 1;
+	void *new = malloc(len);
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	return ((char *)memcpy(new, str, len));
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
