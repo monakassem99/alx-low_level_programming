188f234e477c543e79507ab7c01c2c463c38ee06
monakassem99 <mona.kassem9999@gmail.com>
2023-08-11 02:25:44 +0300

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - prints the lenght of given string
- *
- * @s: the pointer of string
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	int n;
-	int i = 0;
-
-	for (n = 0; s[n] != '\0'; n++)
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * *_memcpy - copy memory area
- * @dest: memory area
- * @src: source
- * @n: length of source
- *
- * Return: dest
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int fll;
-
-	for (fll = 0; fll < n; fll++)
-	{
-		dest[fll] = src[fll];
-	}
-	return (dest);
-}
-
-/**
  * string_nconcat - concatenates two strings.
  * @s1: first string
  * @s2: second string
@@ -49,36 +10,43 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len_1 = 0, len_2 = 0, size = 0;
-	char *fin_str = NULL;
 
-	size = len_1 + len_2 + 1;
-	fin_str = malloc(size);
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-	if (s1 != NULL)
-	{
-		len_1 = _strlen(s1);
-	}
-	if (s2 != NULL)
-	{
-		len_2 = _strlen(s2);
-	}
-	if (n >= len_2)
-	{
-		n = len_2;
-	}
-	if (fin_str == NULL)
-	{
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
-	}
-	if (s1 != NULL)
+
+	k = 0;
+	while (j < l)
 	{
-		_memcpy(fin_str, s1, len_1);
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
 	}
-	if (s2 != NULL)
-	{
-		_memcpy(fin_str + len_1, s2, n);
-	}
-	fin_str[len_1 + n] = '\0';
-	return (fin_str);
+	str[j] = '\0';
+	return (str);
 }
