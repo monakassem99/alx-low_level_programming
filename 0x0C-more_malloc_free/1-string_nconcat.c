2fc4466382b71df3ccc6cfbd0b4631fc41e450e6
monakassem99 <mona.kassem9999@gmail.com>
2023-08-11 01:52:21 +0300

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,43 +10,40 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i, len_1, len_2, size;
+	char *fin_str;
 
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	len_1 = strlen(s1);
+	len_2 = strlen(s2);
+	size = len_1 + len_2 + 1;
+	fin_str = malloc(size);
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
-		i++;
-
-	while (s2[k])
-		k++;
-
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-	while (j < l)
 	{
-		if (j <= i)
-			str[j] = s1[j];
-
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+		s1 = "";
 	}
-	str[j] = '\0';
-	return (str);
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (i = 0; i < len_1; i++)
+	{
+		fin_str[i] = s1[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		fin_str[len_1 + i] = s2[i];
+	}
+
+	fin_str[size - 1] = '\0';
+
+	if (fin_str == NULL)
+	{
+		return (NULL);
+	}
+
+	return (fin_str);
 }
