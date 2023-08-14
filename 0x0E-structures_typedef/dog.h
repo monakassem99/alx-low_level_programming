72525475cc3926febaa09983e9e7567cb8b770d8
monakassem99 <mona.kassem9999@gmail.com>
2023-08-14 23:13:08 +0300

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
+#include <string.h>
 
+/**
+ * struct dog - dog details
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
