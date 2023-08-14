8a0d44281dd097a48fb0222f7b800c1cd6a1dfef
monakassem99 <mona.kassem9999@gmail.com>
2023-08-15 02:26:44 +0300

    updated

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,9 +2,8 @@
 #include "dog.h"
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    /* d = malloc(sizeof(struct dog));*/
-    dog doog;
-    doog.name = name;
-    (*d).age = age;
-    (*d).owner = owner;
+    d = malloc(sizeof(struct dog));
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,13 @@
 
 void print_dog(struct dog *d)
 {
-
+	if (d == NULL)
+		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else if ((isdigit(d->age)))
 		printf("Age: (nil)\n");
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
