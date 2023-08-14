9fdde77bde0f47254311335b9015b124e298869e
monakassem99 <mona.kassem9999@gmail.com>
2023-08-15 01:58:16 +0300

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,8 +1,14 @@
 #include "dog.h"
 
+/**
+ * print_dog -  prints a struct
+ * @d: pointer to struct
+ * Return: nothing
+ */
+
 void print_dog(struct dog *d)
 {
-    printf("Name: %s\n", d->name);
-    printf("Age: %f\n", d->age);
-    printf("Owner: %s\n", d->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
