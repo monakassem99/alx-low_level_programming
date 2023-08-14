be34b17331923974bae6cea42ba7f2888a2b1290
monakassem99 <mona.kassem9999@gmail.com>
2023-08-15 02:32:02 +0300

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
+		d->name = "(nil)";
 	else if (d->owner == NULL)
-		printf("Owner: (nil)\n");
+		d->owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
