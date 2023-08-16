76de784cc665ce89df8c786e9736f19be54b5da8
monakassem99 <mona.kassem9999@gmail.com>
2023-08-16 22:18:36 +0300

    updated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include "_putchar.h"
 
 /**
  * print_name - prints a name
@@ -10,4 +11,55 @@
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
+}
+/**
+ * print_with_newline - njxnm
+ * @name: name
+ * Return: jhnjj
+ */
+void print_with_newline(char *name)
+{
+	while (*name != '\0')
+	{
+		_putchar(*name);
+		name++;
+	}
+	_putchar('\n');
+}
+/**
+ * print_with_dashes - xjnn
+ * @name: name
+ * Return: nnn
+ */
+void print_with_dashes(char *name)
+{
+	int i;
+
+	int length = 0;
+	char *temp = name;
+
+	while (*temp != '\0')
+	{
+		length++;
+		temp++;
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar('-');
+	}
+	_putchar('\n');
+
+	while (*name != '\0')
+	{
+		_putchar(*name);
+		name++;
+	}
+	_putchar('\n');
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar('-');
+	}
+	_putchar('\n');
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
+#include "_putchar.h"
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
 
+void print_name(char *name, void (*f)(char *));
+void print_with_newline(char *name);
+void print_with_dashes(char *name);
 #endif
