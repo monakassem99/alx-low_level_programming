5a5a30822b35b9978a9e42c760c5d1cea9f90682
monakassem99 <mona.kassem9999@gmail.com>
2023-08-18 02:57:23 +0300

    updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,15 +1,27 @@
 #include "variadic_functions.h"
+
+/**
+ * sum_them_all - return sum of all arguments
+ * @n: fixed argument
+ * Return: sum
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int sum;
-    unsigned int i;
-    va_list ptr;
-    if (n == 0)
-        return (0);
-    sum = 0;
-    va_start(ptr, n);
-    for (i = 0; i < n; i++)
-        sum += va_arg(ptr, int);
-    va_end(ptr);
-    return (sum);
+	unsigned int sum;
+	unsigned int i;
+	va_list ptr;
+
+	if (n == 0)
+		return (0);
+
+	sum = 0;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ptr, int);
+	va_end(ptr);
+
+	return (sum);
 }
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -  prints numbers, followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+
+		printf("%d", va_arg(ptr, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(ptr);
+	printf("\n");
+}
