60fcff01d8cb840c66263288e34681daad51f0fa
monakassem99 <mona.kassem9999@gmail.com>
2023-08-18 12:36:56 +0300

    updated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,64 +1,13 @@
 #include "function_pointers.h"
-#include "stdio.h"
+#include <stdlib.h>
 /**
- * print_name - prints a name
- *@name: name
- *@f: function pointer
- * Return: nothing
+ * print_name - prints the name
+ * @name: name
+ * @f: function
  */
-
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
-}
-/**
- * print_with_newline - njxnm
- * @name: name
- * Return: jhnjj
- */
-void print_with_newline(char *name)
-{
-	while (*name != '\0')
-	{
-		_putchar(*name);
-		name++;
-	}
-	_putchar('\n');
-}
-/**
- * print_with_dashes - xjnn
- * @name: name
- * Return: nnn
- */
-void print_with_dashes(char *name)
-{
-	int i;
-
-	int length = 0;
-	char *temp = name;
-
-	while (*temp != '\0')
-	{
-		length++;
-		temp++;
-	}
-
-	for (i = 0; i < length; i++)
-	{
-		_putchar('-');
-	}
-	_putchar('\n');
-
-	while (*name != '\0')
-	{
-		_putchar(*name);
-		name++;
-	}
-	_putchar('\n');
-
-	for (i = 0; i < length; i++)
-	{
-		_putchar('-');
-	}
-	_putchar('\n');
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void print_with_newline(char *name);
-void print_with_dashes(char *name);
+int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 #endif
