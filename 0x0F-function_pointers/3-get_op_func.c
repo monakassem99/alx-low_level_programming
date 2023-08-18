ce37421727771c7e3a06713f107f15274bd3700a
monakassem99 <mona.kassem9999@gmail.com>
2023-08-18 12:38:16 +0300

    updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,21 +8,21 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t op_s[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}};
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
 
-    int i = 0;
+	int i = 0;
 
-    while (op_s[i].op)
-    {
-        if (*(op_s[i].op) == *s)
-            return (op_s[i].f);
-        i++;
-    }
-    return (NULL);
+	while (op_s[i].op)
+	{
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
+		i++;
+	}
+	return (NULL);
 }
