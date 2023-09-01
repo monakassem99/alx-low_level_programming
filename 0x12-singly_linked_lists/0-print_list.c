c88054d423ed839dabedf6351b141967f9f686ea
monakassem99 <mona.kassem9999@gmail.com>
2023-09-01 08:54:28 +0300

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t nodes = 0;
+	const list_t *current = h;
 
-	while (h != NULL)
+	for (; current != NULL; current = current->next, nodes++)
 	{
-		if (h->str == NULL)
+		if (current->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		h = h->next;
-		i++;
+			printf("[%d] %s\n", current->len, current->str);
 	}
-	return (i);
+	return (nodes);
 }
